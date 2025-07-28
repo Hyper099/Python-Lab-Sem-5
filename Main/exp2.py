f378c3ddf269acbe977974693449df909caf7216
Jayneel Mahival <163031986+Hyper099@users.noreply.github.com>
2025-07-28 09:12:00 +0000

    init exp2

--- Main/exp2.py
+++ Main/exp2.py
@@ -1,1 +1,12 @@
-₹
+import csv
+
+def main():
+  with open('../Data/students.csv', mode='r') as f:
+    csvFile = csv.DictReader(f)
+    for lines in csvFile:
+      print(lines)
+
+
+  
+if __name__ == "__main__":
+  main()
